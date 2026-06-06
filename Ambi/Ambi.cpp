4f57639eb4cad962d004d4de1b842f246ff7bd73
Darkoh <lukekirk05@gmail.com>
2026-06-06 15:25:21 +0100

    Implemented CMake. Should be working but see future commits if not

--- Ambi/Ambi.cpp
+++ Ambi/Ambi.cpp
@@ -9,7 +9,7 @@ int main()
 
 	const auto numberOfThreads = std::thread::hardware_concurrency();	// Let developer know how many threads the device has, so they can decide how many threads to use for their application.
 
-	std::cout << "Number of threads on device: " << numberOfThreads << std::endl;
+	std::cout << "Number of threads on this device: " << numberOfThreads << std::endl;
 
 	while (running)
 	{
