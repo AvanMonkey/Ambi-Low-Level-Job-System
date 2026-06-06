7e3595a2f7afbbc027cb3609e3fb39cc2da5778a
Darkoh <lukekirk05@gmail.com>
2026-06-06 14:58:04 +0100

    Print number of threads device has to terminal at start of development

--- Ambi/Ambi.cpp
+++ Ambi/Ambi.cpp
@@ -2,8 +2,18 @@
 //
 
 #include <iostream>
-
+#include <thread>
 int main()
 {
-    std::cout << "Hello World!\n";
+	bool running = true;
+
+	const auto numberOfThreads = std::thread::hardware_concurrency();	// Let developer know how many threads the device has, so they can decide how many threads to use for their application.
+
+	std::cout << "Number of threads on device: " << numberOfThreads << std::endl;
+
+	while (running)
+	{
+		break;
+	}
+	return 0;
 }
